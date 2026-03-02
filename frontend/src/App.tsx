81406c16b995cc10fde359e2b9feda997582d6ab
Madhur <madhurchaturvedi2000@gmail.com>
2026-03-02 16:31:20 +0530

    googel frontend integraton done bug accaurs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import Login from "./pages/Login"
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import {Toaster} from "react-hot-toast";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Toaster />
    </BrowserRouter>
    </>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+export const authService = "http://localhost:5000";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="540131442119-rnepenl98kli7csj5ort6t4plr0qtbq2.apps.googleusercontent.com">
+       <App />
+    </GoogleOAuthProvider>;
   </StrictMode>,
 )
